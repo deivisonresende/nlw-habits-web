4776db55442ab5f1cb627a81368130790965be48
deivison resende <deivison.resende@enacom.com.br>
2023-01-22 22:23:22 -0300

    feat: sent habit form and added form validations

--- src/views/home/components/AddHabitForm.tsx
+++ src/views/home/components/AddHabitForm.tsx
@@ -1,6 +1,8 @@
 import { Check } from "phosphor-react";
 import * as Checkbox from '@radix-ui/react-checkbox';
 import { FormEvent, useState } from 'react'
+import { api } from "../../../lib/axios";
+import clsx from "clsx";
 
 const availableWeekDays = [
   'Domingo',
@@ -14,10 +16,24 @@ const availableWeekDays = [
 
 export function AddHabitForm(){
   const [title, setTitle] = useState('')
+  const [submitted, setSubmitted] = useState(false)
   const [weekDays, setWeekDays] = useState<Number[]>([])
 
-  const handleSubmitNewHabit = (event: FormEvent) => {
+  const handleSubmitNewHabit = async (event: FormEvent) => {
     event.preventDefault()
+    if(!title.length || !weekDays.length){
+      return
+    }
+
+    setSubmitted(true)
+    await api.post('habits', {
+      title,
+      weekDays
+    })
+
+    setSubmitted(false)
+    setTitle('')
+    setWeekDays([])
   }
 
   const handleToggleWeekDays = (targetDay : number) => {
@@ -38,8 +54,16 @@ export function AddHabitForm(){
         type="text" 
         id="title"
         placeholder="ex.: Exercícios, dormir bem, etc..."
-        className="bg-zinc-800 rounded-lg placeholder:text-zinc-400 mt-3 p-4 outline-none"
+        className={
+          clsx(
+            'bg-zinc-800 rounded-lg placeholder:text-zinc-400 mt-3 p-4 outline-none',
+            {
+              'border border-red-500': !title.length && submitted
+            }
+          )
+        }
         onChange={event => setTitle(event.target.value)}
+        value={title}
         autoFocus
       />
 
@@ -49,23 +73,36 @@ export function AddHabitForm(){
       >
         Qual a recorrência ?
       </label>
+      
+      {
+        submitted && !weekDays.length 
+        ? <span className="font-thin text-sm text-red-500 mt-2"> Selecione pelo menos um dia da semana </span> 
+        : null
+      }
 
       <div className="mt-3 flex flex-col gap-2">
         {
           availableWeekDays.map((day, i) => (
             <Checkbox.Root 
-              key={day} 
+              key={day}
+              checked={weekDays.includes(i)}
               className='flex items-center gap-3 group'
               onCheckedChange={() => handleToggleWeekDays(i)}
             >
             <div 
-              className='
-                h-8 w-8
-                rounded-lg 
-                flex items-center justify-center 
-                bg-zinc-900 border-2 
-                border-zinc-800 
-                group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'
+              className= {
+                clsx(
+                  `h-8 w-8
+                  rounded-lg 
+                  flex items-center justify-center 
+                  bg-zinc-900 border-2 
+                  border-zinc-800 
+                  group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500`,
+                  {
+                    'border border-red-600': !weekDays.length && submitted
+                  }
+                )
+              }
             >
               <Checkbox.Indicator>
                 <Check size={20}/>
@@ -82,7 +119,14 @@ export function AddHabitForm(){
 
       <button
         type="submit"
-        className="mt-6 rounded-lg flex items-center gap-3 justify-center font-semibold bg-green-600 p-4 hover:bg-green-500"
+        className={
+          clsx(
+            "mt-6 rounded-lg flex items-center gap-3 justify-center font-semibold bg-green-600 p-4 hover:bg-green-500",
+            {
+              'opacity-50 hover:bg-green-600 cursor-no-drop': !weekDays.length || !title.length
+            }
+          )
+        }
       >
         <Check size={20} weight='bold'/>
         Confirmar
