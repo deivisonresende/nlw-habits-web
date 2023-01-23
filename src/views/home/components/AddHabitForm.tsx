9a3be89e3a4361bcd4009ef19fabf17f32fe9901
deivison resende <deivison.resende@enacom.com.br>
2023-01-22 21:01:43 -0300

    feat: added create habit form

--- src/views/home/components/AddHabitForm.tsx
+++ src/views/home/components/AddHabitForm.tsx
@@ -1,8 +1,33 @@
 import { Check } from "phosphor-react";
+import * as Checkbox from '@radix-ui/react-checkbox';
+import { FormEvent, useState } from 'react'
+
+const availableWeekDays = [
+  'Domingo',
+  'Segunda-feira',
+  'Terça-feira',
+  'Quarta-feira',
+  'Quinta-feira',
+  'Sexta-feira',
+  'Sábado'
+]
 
 export function AddHabitForm(){
+  const [title, setTitle] = useState('')
+  const [weekDays, setWeekDays] = useState<Number[]>([])
+
+  const handleSubmitNewHabit = (event: FormEvent) => {
+    event.preventDefault()
+  }
+
+  const handleToggleWeekDays = (targetDay : number) => {
+    const alreadySelected = weekDays.includes(targetDay)
+    if(!alreadySelected) setWeekDays([...weekDays, targetDay])
+    else setWeekDays(weekDays.filter(day => day !== targetDay))
+  }
+
   return (
-    <form className="flex flex-col mt-6">
+    <form onSubmit={handleSubmitNewHabit} className="flex flex-col mt-6">
       <label 
         htmlFor='title'
         className="font-semibold leading-tight"
@@ -14,6 +39,7 @@ export function AddHabitForm(){
         id="title"
         placeholder="ex.: Exercícios, dormir bem, etc..."
         className="bg-zinc-800 rounded-lg placeholder:text-zinc-400 mt-3 p-4 outline-none"
+        onChange={event => setTitle(event.target.value)}
         autoFocus
       />
 
@@ -23,6 +49,36 @@ export function AddHabitForm(){
       >
         Qual a recorrência ?
       </label>
+
+      <div className="mt-3 flex flex-col gap-2">
+        {
+          availableWeekDays.map((day, i) => (
+            <Checkbox.Root 
+              key={day} 
+              className='flex items-center gap-3 group'
+              onCheckedChange={() => handleToggleWeekDays(i)}
+            >
+            <div 
+              className='
+                h-8 w-8
+                rounded-lg 
+                flex items-center justify-center 
+                bg-zinc-900 border-2 
+                border-zinc-800 
+                group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'
+            >
+              <Checkbox.Indicator>
+                <Check size={20}/>
+              </Checkbox.Indicator>
+            </div>
+            <span className='leading-tight'> 
+              { day }
+            </span>
+          </Checkbox.Root>
+          ))
+        }
+      
+      </div>
 
       <button
         type="submit"
