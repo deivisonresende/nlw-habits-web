e1e14e9bab3502aafb2f44464aee4ebf7fdc4edb
deivison resende <deivison.resende@enacom.com.br>
2023-01-22 21:02:41 -0300

    feat: added dynmic backgroud for habit days of summary table

--- src/views/home/components/SummaryTable.tsx
+++ src/views/home/components/SummaryTable.tsx
@@ -25,7 +25,12 @@ export function SummaryTable(){
       </div>
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {
-          summaryDates.map(date => <HabitDay key={date.toString()} />)
+          summaryDates.map(date => <HabitDay 
+            key={date.toString()}
+            amount={50} 
+            completed={Math.round(Math.random() * 50)} 
+            />
+          )
         }
 
         { 
